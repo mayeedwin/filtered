5a87ccfb47b6a2104dcd6790343af3327462af1f
Maye Edwin <em@sky.garden>
2021-02-11 11:37:37 +0300

    Updated param name

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@
 interface Filter {
   data: any[];
   filter: string;
-  value: string;
+  query: string;
 }
 
-const filtered = ({ data, filter, value }: Filter) => {
+const filtered = ({ data, filter, query }: Filter) => {
   if (data.length > 0) {
     const found = data.filter((item: any) => {
       let isFound = false;
       const filter_value = item[`${filter}`];
       if (filter_value) {
-        if (filter_value.toLowerCase().indexOf(value) !== -1) {
+        if (filter_value.toLowerCase().indexOf(query) !== -1) {
           isFound = true;
         }
-        if (filter_value.indexOf(value) !== -1) {
+        if (filter_value.indexOf(query) !== -1) {
           isFound = true;
         }
       }
