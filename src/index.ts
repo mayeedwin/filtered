aa0f43e9c146687cf719b7ec2e200971ede19717
Maye Edwin <em@sky.garden>
2021-02-17 22:49:58 +0300

    New API Release : Filter firestore collections without destructuring docs into an []

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,36 @@ interface Filter {
 }
 
 const filtered = ({ data, filter, query, firestore = false }: Filter) => {
-  // Check if firestore...
-  const db: any[] = [];
-  // Destructure firestore doc....
-  firestore ? data.forEach((doc) => db.push(doc.data())) : null;
-  const toQuery = firestore ? db : data;
+  // Destructure irestore doc....
   if (data.length > 0) {
-    const found = toQuery.filter((item: any) => {
-      let isFound = false;
-      const filter_value = item[`${filter}`];
-      if (filter_value) {
-        if (filter_value.toLowerCase().indexOf(query) !== -1) {
-          isFound = true;
-        }
-        if (filter_value.indexOf(query) !== -1) {
-          isFound = true;
-        }
-      }
-      return isFound;
-    });
+    const found = firestore
+      ? data.filter((item: any) => {
+          let isFound = false;
+          const doc = item.data();
+          const filter_value = doc[`${filter}`];
+          if (filter_value) {
+            if (filter_value.toLowerCase().indexOf(query) !== -1) {
+              isFound = true;
+            }
+            if (filter_value.indexOf(query) !== -1) {
+              isFound = true;
+            }
+          }
+          return isFound;
+        })
+      : data.filter((item: any) => {
+          let isFound = false;
+          const filter_value = item[`${filter}`];
+          if (filter_value) {
+            if (filter_value.toLowerCase().indexOf(query) !== -1) {
+              isFound = true;
+            }
+            if (filter_value.indexOf(query) !== -1) {
+              isFound = true;
+            }
+          }
+          return isFound;
+        });
     return found;
   }
 };
